238c27bbae8edd7289a763e07ee8549ab8b92443
Jakub Schulz <jschulz@f24holding.org>
2022-02-27 20:46:30 +0100

    Netlify deploy config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,60 +5,63 @@ import styleResourcesConfig from './config/style-resources';
 import dayjsConfig from './config/dayjs';
 
 const config: NuxtConfig = {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'bojkotuj-rosyjskie-produkty',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { href: 'https://fonts.googleapis.com', rel: 'preconnect' },
-      { href: 'https://fonts.gstatic.com', rel: 'preconnect', crossorigin: '' },
-      { href: 'https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap', rel: 'stylesheet' },
-    ],
-  },
+	target: 'static',
+	generate: {
+		fallback: true,
+	},
+	head: {
+		title: 'bojkotuj-rosyjskie-produkty',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: '' },
+			{ name: 'format-detection', content: 'telephone=no' },
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{ href: 'https://fonts.googleapis.com', rel: 'preconnect' },
+			{ href: 'https://fonts.gstatic.com', rel: 'preconnect', crossorigin: '' },
+			{ href: 'https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap', rel: 'stylesheet' },
+		],
+	},
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/assets/styles/global.scss',
-  ],
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [
+		'~/assets/styles/global.scss',
+	],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [
+	],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: false,
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: false,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-    ['@nuxtjs/style-resources', styleResourcesConfig],
-  ],
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+		// https://go.nuxtjs.dev/typescript
+		'@nuxt/typescript-build',
+		['@nuxtjs/style-resources', styleResourcesConfig],
+	],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-    ['@nuxtjs/firebase', firebaseConfig],
-    ['@nuxtjs/dayjs', dayjsConfig],
-  ],
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [
+		// https://go.nuxtjs.dev/pwa
+		'@nuxtjs/pwa',
+		['@nuxtjs/firebase', firebaseConfig],
+		['@nuxtjs/dayjs', dayjsConfig],
+	],
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
-  pwa: {
-    manifest: {
-      lang: 'en',
-    },
-  },
+	// PWA module configuration: https://go.nuxtjs.dev/pwa
+	pwa: {
+		manifest: {
+			lang: 'en',
+		},
+	},
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {
+	},
 };
 
 export default config;
